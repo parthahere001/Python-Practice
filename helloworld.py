2eef51ad43cf073beb0311886890e40e7f234642
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2022-04-22 22:28:56 +0530

    Hello World Program
    The first program which supposedly says Hello World!

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+#Program to print Hello World, but you know what, I don't want to keep printing the samething thing everytime I start learning a new language, so I will print something else
+print("Something Else")
