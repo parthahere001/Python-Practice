f1b475e8c925c39536e140a9e368303f88b4ae19
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2022-04-29 14:07:11 +0530

    Loops In Python
    For Loop And While Loop In Python

---
+++ For_Loop.py
@@ -0,0 +1,3 @@
+#Program To Print The Values 1 to 10 Using A For Loop In Python
+for i in range(1,9):
+ print(i)
---
+++ While_Loop.py
@@ -0,0 +1,6 @@
+#Program To Show The Use Of While Loop In Python By Printing The Numbers 1 To 8
+i=0
+while(i<8):
+  i+=1
+  print(i)
+
