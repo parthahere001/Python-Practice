dbea69d25f5cef9f745264a8d1ce50ccbb513071
parthahere001 <101795082+parthahere001@users.noreply.github.com>
2022-05-01 11:22:25 +0530

    Add files via upload

---
+++ ARRAYS.py
@@ -0,0 +1,6 @@
+arr=[]
+for i in range(0,4):
+ p=i+1
+ arr.append(p)
+for i in range(0,4):
+ print(arr[i])
---
+++ Data_Types.py
@@ -0,0 +1,7 @@
+#Program To Show The Different Data Types In Python
+k = 10
+e = 1000.000
+g= 121.10
+h= 121.1002
+w= "A Line"
+print(w,k,e,g,h)
